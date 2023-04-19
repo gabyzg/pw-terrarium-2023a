f188b95c17be420db7907fc09de7bad1586da526
gabyzg <100975074+gabyzg@users.noreply.github.com>
2023-04-19 18:30:36 +0000

    🤩Controlando elementos

---
+++ script.js
@@ -0,0 +1,14 @@
+//tomando control del objeto 
+let plant1 = document.getElementById("plant1")
+
+//
+plant1.addEventListener("click",()=>{
+    let name = prompt("Hola, ¿cual es tu nombre?");
+    if(name !== ""){
+    alert(`${name}save the planet!!!!!!🌻🌾`);
+    }
+});
+
+
+
+ 
