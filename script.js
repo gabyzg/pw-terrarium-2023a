2fd70e5f21c4689afa7c36001846de7412f78336
gabyzg <100975074+gabyzg@users.noreply.github.com>
2023-04-19 19:05:47 +0000

    🏗️building dragging funcition

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-//tomando control del objeto 
-let plant1 = document.getElementById("plant1")
+//hacinedo que la plnata 1 de mi documento
+//sea arrastrable
+dragElement(document.getElementById("plant1"));
 
-//
-plant1.addEventListener("click",()=>{
-    let name = prompt("Hola, ¿cual es tu nombre?");
-    if(name !== ""){
-    alert(`${name}save the planet!!!!!!🌻🌾`);
-    }
-});
+//implementar la funcion dragelement
 
+function dragElement(terrariumElement){
+  //CReando variables que controlan las posiciones 
+  //iniciales y finales
+  let pos1 = 0,
+  pos2 =0,
+  pos3 =0,
+  pos4 =0
 
+  //Registrar un evento 
+  terrariumElement.onpointerdown = pointerDrag
 
- 
+function pointerDrag(event){
+    //previene todo comportamiento que tenga por defecto 
+    // Previene todo comportamiento
+        // que tenga por defecto
+        // el evento en cuestion en
+        // mi documento
+    event.preventDefault();
+    console.log("+ on pointer down");
+
+}
+
+  
+}
